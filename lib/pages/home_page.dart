4fd6e4caa44d9da08cc24e6a6dcbc8493ef398f3
Lunix-420 <davidhess336@gmx.de>
2025-04-06 22:30:23 +0200

    refactor: replace TodoList with TodoTile and update UI with catppuccin theme

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todo/widgets/todo_list.dart';
+import 'package:todo/widgets/todo_tile.dart';
+import 'package:catppuccin_flutter/catppuccin_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,9 +10,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Flavor flavor = catppuccin.macchiato;
+
   final _controller = TextEditingController();
   List toDoList = [
-    ['Flutter Lernen', true],
+    ['Flutter Lernen', false],
     ['Kaffee trinken', false],
     ['Buch lesen', false],
     ['Film schauen', false],
@@ -30,23 +33,30 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple.shade300,
+      backgroundColor: flavor.base,
       appBar: AppBar(
         title: const Text('Todo Liste'),
         centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        foregroundColor: Colors.white,
+        backgroundColor: flavor.surface0,
+        foregroundColor: flavor.text,
       ),
       body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (BuildContext context, index) {
-          return TodoList(
+          return TodoTile(
             taskName: toDoList[index][0],
             taskDone: toDoList[index][1],
             onChanged: (value) => checkBoxChanged(index),
+            onDelete: () => deleteTask(index),
           );
         },
       ),
@@ -57,26 +67,41 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.only(left: 30, right: 10),
               child: TextField(
                 controller: _controller,
-                cursorColor: Colors.deepPurple,
-                style: TextStyle(color: Colors.black),
+                cursorColor: flavor.text,
+                style: TextStyle(color: flavor.text),
                 decoration: InputDecoration(
                   hintText: "Neue Aufgabe hinzufügen",
-                  hintStyle: TextStyle(color: Colors.deepPurple),
+                  hintStyle: TextStyle(color: flavor.subtext0),
                   filled: true,
-                  fillColor: Colors.deepPurple.shade200,
+                  fillColor: flavor.crust,
                   enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple),
+                    borderSide: BorderSide(color: flavor.surface0),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple),
+                    borderSide: BorderSide(color: flavor.surface0),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
               ),
             ),
           ),
-          FloatingActionButton(onPressed: addTask, child: Icon(Icons.add)),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [flavor.peach, flavor.red],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              shape: BoxShape.circle,
+            ),
+            child: FloatingActionButton(
+              onPressed: addTask,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              child: Icon(Icons.add),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/widgets/todo_tile.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:catppuccin_flutter/catppuccin_flutter.dart';
+
+class TodoTile extends StatelessWidget {
+  const TodoTile({
+    super.key,
+    required this.taskName,
+    required this.taskDone,
+    this.onChanged,
+    this.onDelete,
+  });
+
+  final String taskName;
+  final bool taskDone;
+  final Function(bool?)? onChanged;
+  final VoidCallback? onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    Flavor flavor = catppuccin.macchiato;
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 0),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: flavor.surface0,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              value: taskDone,
+              onChanged: onChanged,
+              checkColor: flavor.crust,
+              activeColor: flavor.text,
+              side: BorderSide(color: flavor.text),
+            ),
+            Expanded(
+              child: Text(
+                taskName,
+                style: TextStyle(
+                  color: flavor.text,
+                  fontSize: 18,
+                  decoration:
+                      taskDone
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                  decorationColor: flavor.text,
+                  decorationThickness: 2,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: onDelete, // Call the onDelete callback
+              icon: Icon(Icons.delete, color: flavor.red),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
